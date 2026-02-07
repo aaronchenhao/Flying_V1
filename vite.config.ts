dc6a587950fe47ccd409ba2c74208e808f0efbfb
Hao Chen <chenhaohz@126.com>
2026-02-07 15:44:29 +0800

    Update files for tailwind migration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   server: {
@@ -13,7 +12,7 @@ export default defineConfig({
       'Access-Control-Allow-Origin': '*'
     }
   },
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
